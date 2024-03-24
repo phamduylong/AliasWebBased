f84e38a4e4f6e30eb4cb7c75e8ecab58a5d35605
longph <longphamduy2002@gmail.com>
2024-03-24 20:07:11 +0200

    Initial commit & landing page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+// @ts-nocheck
+
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Alias Web Game</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://longph.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Author
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/phamduylong"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<LightSwitch class="my-4 ml-auto mr-4"/>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>

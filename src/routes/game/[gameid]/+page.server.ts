2ea6cea1093ed626e0a2c43cef8dfcb55f717c9d
longph <longphamduy2002@gmail.com>
2024-08-21 22:57:05 +0300

    New import style again

--- src/routes/game/[gameid]/+page.server.ts
+++ src/routes/game/[gameid]/+page.server.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ }) {
     const data: Word[] = [];
-    const url = new URL(`${import.meta.url}../../../../../lib/assets/words_version6.csv`);
+    const url = new URL(`${import.meta.url}/../../../../lib/assets/words_version6.csv`);
     try {
         const content = fs.promises.readFile(url, 'latin1');
         const lines = (await content).toString().split(EOL);

0c100db55dc10a18f71eda6189032007df673068
longph <longphamduy2002@gmail.com>
2024-08-31 19:40:06 +0300

    POST request to update game state between turns

--- src/routes/game/[gameid]/+server.ts
+++ src/routes/game/[gameid]/+server.ts
@@ -1,15 +1,28 @@
-import { redirect, error } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
+import type { Game } from "$lib/types";
+import { ClientResponseError } from "pocketbase";
+import { validateGame } from "$lib/helpers/common";
 export const POST: RequestHandler = async ({ locals, request }) => {
-    try {
-        const gamesCollection = locals.pocketBase.collection("games");
-        const currentGame = await gamesCollection.getListFirstItem({game_id: request.params.gameid});
-        const game = await request.json();
-        if (!currentGame) {
-            throw error(404, "Game not found");
-        }
-
-    } catch(e) {
-        throw e
+  const gamesCollection = locals.pocketBase.collection("games");
+  const game : Game = await request.json();
+  try {
+    const currGame = await gamesCollection.getFirstListItem(
+        `game_id="${game.game_id}"`
+      );
+      currGame.words = game.words;
+      currGame.team1_score = game.team1_score;
+      currGame.team2_score = game.team2_score;
+      const errors = validateGame(currGame);
+      if (errors.length !== 0) {
+        throw error(400, errors.join(", "));
+      }
+    gamesCollection.update<Game>(currGame.id, currGame);
+  } catch (err) {
+    if (err instanceof ClientResponseError) {
+      throw error(err.status, err.message);
     }
+    throw err;
+  }
+  redirect(303, `/game/${game.game_id}`);
 }

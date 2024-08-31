a41cf0fd02b169a39cbbfc09bae6f94a7226e3dd
longph <longphamduy2002@gmail.com>
2024-08-31 19:41:22 +0300

    Moving types to a new separate file

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+export type Word = {
+    word: string;
+    shown: boolean;
+}
+
+export type Game = {
+    game_id: string;
+    team1: string;
+    team2: string;
+    team1_score: number;
+    team2_score: number;
+    words: Word[];
+}

08fbac1e34b54b5420c50ce35a8b920221f5f06c
longph <longphamduy2002@gmail.com>
2024-08-31 19:44:15 +0300

    Add type

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -1,32 +1,31 @@
 import { redirect, error } from '@sveltejs/kit';
-import type {PageServerLoad} from './$types';
 import { randomUUID } from 'crypto';
-export const load = (async () => {
-    return {team1: "", team2: "", words: ""};
-}) satisfies PageServerLoad;
-
+import type { Game } from '$lib/types';
+import { validateGame } from '$lib/helpers/common';
+import { team1Turn } from '$lib/teamsTurn.js';
 export const actions = {
-    default: async ({ locals, request }) => {
-        const gameId = randomUUID();
-        const formData = await request.formData();
-        try {
-            let words = JSON.parse(formData.get("words") as string);
-            for(let i = 0; i < words.length; i++) {
-                words[i] = {word: words[i], shown: false};
-            }
-            const newGame = {
-                game_id: gameId,
-                team1: formData.get("team-1"),
-                team2: formData.get("team-2"),
-                team1_score: 0,
-                team2_score: 0,
-                words: words,
-            }
-            const gamesCollection = locals.pocketBase.collection("games");
-            await gamesCollection.create(newGame);
-        } catch (e) {
-            throw error(500, (e as Error).toString());
-        }
-        throw redirect(303, `/game/${gameId}`);
-    }
-}
+	default: async ({ locals, request }) => {
+		const gameId = randomUUID();
+		const formData = await request.formData();
+		let words = JSON.parse(formData.get('words') as string);
+		for (let i = 0; i < words.length; i++) {
+			words[i] = { word: words[i], shown: false };
+		}
+		const newGame: Game = {
+			game_id: gameId,
+			team1: formData.get('team-1') as string,
+			team2: formData.get('team-2') as string,
+			team1_score: 0,
+			team2_score: 0,
+			words: words
+		};
+		const errors = validateGame(newGame);
+		if (errors.length !== 0) {
+			throw error(400, errors.join(', '));
+		}
+		const gamesCollection = locals.pocketBase.collection('games');
+		await gamesCollection.create(newGame);
+		team1Turn.switchToTeam1();
+		throw redirect(303, `/game/${gameId}`);
+	}
+};

f0a06b24a2dce19ada69f4185f469c6f9289b639
longph <longphamduy2002@gmail.com>
2024-09-02 22:42:30 +0300

    Migration for db structure

---
+++ pocketbase/pb_migrations/1725215668_updated_games.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("uk0f12226b3de0i")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "t6jwx3bf",
+    "name": "is_team1_turn",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("uk0f12226b3de0i")
+
+  // remove
+  collection.schema.removeField("t6jwx3bf")
+
+  return dao.saveCollection(collection)
+})

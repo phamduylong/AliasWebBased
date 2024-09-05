a4d709b6e5a556ba026c594eb757930e9f913520
longph <longphamduy2002@gmail.com>
2024-09-05 23:58:14 +0300

    Load theme from cookies on hook handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,9 +8,24 @@ export const handle = async ({ event, resolve }) => {
 
 	event.locals.pocketBase.authStore.loadFromCookie(event.request.headers.get('cookie') ?? '');
 
-	const response = await resolve(event);
+	let theme = '';
 
-	response.headers.set(
+	const cookieTheme = event.cookies.get('theme');
+
+	if (cookieTheme) {
+		theme = cookieTheme;
+	} else {
+		event.cookies.set('theme', 'skeleton', { path: '/' });
+		theme = 'skeleton';
+	}
+
+	const response = await resolve(event, {
+		transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
+	});
+
+	// Use headers.append so that it doesn't overwrite existing cookies for theme
+	// See: https://github.com/remix-run/remix/issues/231
+	response.headers.append(
 		'set-cookie',
 		event.locals.pocketBase.authStore.exportToCookie({ secure: false })
 	);

ded7c98af13f45980dd4f35176019eb42eae60ae
longph <longphamduy2002@gmail.com>
2024-09-06 14:56:17 +0300

    Quick UI patch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,12 @@
 	import '../app.postcss';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { browser } from '$app/environment';
-	import { CircleHelp, Github, ArrowDown, Palette } from 'lucide-svelte';
+	import { CircleHelp, Code, ArrowDown, Palette } from 'lucide-svelte';
 	import {
 		initializeStores,
 		popup,
 		storePopup,
 		AppBar,
-		AppShell,
 		LightSwitch,
 		Toast
 	} from '@skeletonlabs/skeleton';
@@ -48,79 +47,77 @@
 
 <!-- Singleton Toast -->
 <Toast />
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<a href="/"><strong class="text-xl uppercase">Alias Web Game</strong></a>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<!-- Theme -->
-				<div>
-					<!-- trigger -->
-					<button
-						class="btn hover:variant-soft-primary"
-						use:popup={{ event: 'click', target: 'theme', closeQuery: 'a[href]' }}
-					>
-						<Palette class="md:!hidden" />
-						<span class="hidden md:inline-block">Theme </span>
-						<ArrowDown class="hidden md:inline-block" />
-					</button>
-					<!-- popup -->
-					<div class="card p-4 w-60 shadow-xl z-40" data-popup="theme">
-						<div class="space-y-4">
-							<section class="flex justify-between items-center">
-								<h6 class="h6">Mode</h6>
-								<LightSwitch />
-							</section>
-							<hr />
-							<nav class="list-nav p-4 -m-4 max-h-64 lg:max-h-[500px] overflow-y-auto">
-								<form action="/?/setTheme" method="POST" use:enhance={setTheme}>
-									<ul>
-										{#each themes as { icon, name, type }}
-											<li>
-												<button
-													class="option w-full h-full"
-													type="submit"
-													name="theme"
-													value={type}
-													class:bg-primary-active-token={$storeTheme === type}
-												>
-													<span>{icon}</span>
-													<span class="flex-auto text-left">{name}</span>
-												</button>
-											</li>
-										{/each}
-									</ul>
-								</form>
-							</nav>
-						</div>
+<main>
+	<!-- App Bar -->
+	<AppBar>
+		<svelte:fragment slot="lead">
+			<a href="/"><strong class="text-xl uppercase">Alias Web Game</strong></a>
+		</svelte:fragment>
+		<svelte:fragment slot="trail">
+			<!-- Theme -->
+			<div>
+				<!-- trigger -->
+				<button
+					class="btn hover:variant-soft-primary"
+					use:popup={{ event: 'click', target: 'theme', closeQuery: 'a[href]' }}
+				>
+					<Palette class="md:!hidden" />
+					<span class="hidden md:inline-block">Theme </span>
+					<ArrowDown class="hidden md:inline-block" />
+				</button>
+				<!-- popup -->
+				<div class="card p-4 w-60 shadow-xl z-40" data-popup="theme">
+					<div class="space-y-4">
+						<section class="flex justify-between items-center">
+							<h6 class="h6">Mode</h6>
+							<LightSwitch />
+						</section>
+						<hr />
+						<nav class="list-nav p-4 -m-4 max-h-64 lg:max-h-[500px] overflow-y-auto">
+							<form action="/?/setTheme" method="POST" use:enhance={setTheme}>
+								<ul>
+									{#each themes as { icon, name, type }}
+										<li>
+											<button
+												class="option w-full h-full"
+												type="submit"
+												name="theme"
+												value={type}
+												class:bg-primary-active-token={$storeTheme === type}
+											>
+												<span>{icon}</span>
+												<span class="flex-auto text-left">{name}</span>
+											</button>
+										</li>
+									{/each}
+								</ul>
+							</form>
+						</nav>
 					</div>
 				</div>
-				<a class="btn hover:variant-soft-primary hidden md:inline-block" href="/faq">
-					<span class="hidden md:inline-block">Help</span>
-				</a>
-				<a class="btn hover:variant-soft-primary md:hidden" href="/faq">
-					<CircleHelp class="md:hidden" />
-				</a>
-				<a
-					class="btn hover:variant-soft-primary hidden md:inline-block"
-					href="https://github.com/phamduylong/AliasWebBased"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<span class="hidden md:inline-block">GitHub</span>
-				</a>
-				<a
-					href="https://github.com/phamduylong/AliasWebBased"
-					target="_blank"
-					rel="noreferrer"
-					class="btn hover:variant-soft-primary md:hidden"><Github class="md:hidden" size={20} /></a
-				>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<slot />
-</AppShell>
+			</div>
+			<a class="btn hover:variant-soft-primary hidden md:inline-block" href="/faq">
+				<span class="hidden md:inline-block">Help</span>
+			</a>
+			<a class="btn hover:variant-soft-primary md:hidden" href="/faq">
+				<CircleHelp class="md:hidden" />
+			</a>
+			<a
+				class="btn hover:variant-soft-primary hidden md:inline-block"
+				href="https://github.com/phamduylong/AliasWebBased"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<span class="hidden md:inline-block">GitHub</span>
+			</a>
+			<a
+				href="https://github.com/phamduylong/AliasWebBased"
+				target="_blank"
+				rel="noreferrer"
+				class="btn hover:variant-soft-primary md:hidden"><Code class="md:hidden" size={20} /></a
+			>
+		</svelte:fragment>
+	</AppBar>
+</main>
+
+<slot />

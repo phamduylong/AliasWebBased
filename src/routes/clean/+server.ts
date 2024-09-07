48b9f7d029de04f897ad28e2f1ba14dc74ab44de
longph <longphamduy2002@gmail.com>
2024-09-07 12:19:41 +0300

    Fix cron to work properly

--- src/routes/clean/+server.ts
+++ src/routes/clean/+server.ts
@@ -1,22 +1,28 @@
 import { POCKETBASE_URL, VERCEL_CRON_SECRET } from '$env/static/private';
 import PocketBase from 'pocketbase';
-export async function GET(req: Request, res: Response) {
-    console.log("Running from cron");
-    if(req.headers.get('Authorization') !== `Bearer ${VERCEL_CRON_SECRET}`) return new Response('Unauthorized', { status: 401 });
-    const pb = new PocketBase(POCKETBASE_URL);
-    const games = pb.collection('games');
+import type { Game } from '$lib/types';
+export async function GET({ request }) {
+    try {
+        if(request.headers.get('Authorization') !== `Bearer ${VERCEL_CRON_SECRET}`) return new Response('Unauthorized', { status: 401 });
+        const pb = new PocketBase(POCKETBASE_URL);
+        const games = pb.collection('games');
+    
+        // Get date 2 days ago in format 'YYYY-MM-DD HH:MM:SS'
+        const date = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
+        const ymd = date.toISOString().split('T')[0];                       // 'YYYY-MM-DD'
+        const hms = date.toISOString().split('T')[1].substring(0, 8);       // 'HH:MM:SS'
+        const filterDateString = `${ymd} ${hms}`;
 
-    // Get date 2 days ago in format 'YYYY-MM-DD HH:MM:SS'
-    const date = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
-    const ymd = date.toISOString().split('T')[0];
-    const hms = date.toISOString().split('T')[1].substring(0, 8);
-    const filterDateString = `${ymd} ${hms}`;
-    if(!games) return new Response('Database failure: Games collection is missing from database.', { status: 500 });
-    const expiredGames = await games.getFullList({ filter: `created <=  ${filterDateString}` });
-    if(expiredGames.length === 0) return new Response('No games to delete.', { status: 200 });
-    expiredGames.forEach(async game => {
-        await games.delete(game.id);
-    });
-
-    return new Response('Games deleted.', { status: 200 });
+        if(!games) return new Response('Database failure: Games collection is missing from database.', { status: 500 });
+        const expiredGames = await games.getFullList<Game>({ filter: `created <=  "${filterDateString}"` });
+        if(expiredGames.length === 0) return new Response('No games to delete.', { status: 200 });
+        expiredGames.forEach(async game => {
+            await games.delete(game.id);
+        });
+        
+        return new Response('Games deleted.', { status: 200 });
+    } catch (err) {
+        console.error((err as Error).stack);
+        return new Response((err as Error).message, { status: 500 });
+    }
 }

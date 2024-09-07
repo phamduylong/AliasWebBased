4869c1508072d4d0fc8a03fdb77169a4992b326e
longph <longphamduy2002@gmail.com>
2024-09-07 12:50:21 +0300

    Use pocketbase instance created in locals

--- src/routes/clean/+server.ts
+++ src/routes/clean/+server.ts
@@ -1,15 +1,13 @@
-import { POCKETBASE_URL, VERCEL_CRON_SECRET } from '$env/static/private';
-import PocketBase from 'pocketbase';
+import { VERCEL_CRON_SECRET } from '$env/static/private';
 import type { Game } from '$lib/types';
-export async function GET({ request }) {
+export async function GET({ locals, request }) {
     try {
         if(request.headers.get('Authorization') !== `Bearer ${VERCEL_CRON_SECRET}`) return new Response('Unauthorized', { status: 401 });
-        const pb = new PocketBase(POCKETBASE_URL);
-        const games = pb.collection('games');
+        const games = locals.pocketBase.collection('games');
     
         // Get date 2 days ago in format 'YYYY-MM-DD HH:MM:SS'
         const date = new Date(Date.now() - 2 * 24 * 60 * 60 * 1000);
-        const ymd = date.toISOString().split('T')[0];                       // 'YYYY-MM-DD'
+        const ymd = date.toISOString().split('T')[0];                        // 'YYYY-MM-DD'
         const hms = date.toISOString().split('T')[1].substring(0, 8);       // 'HH:MM:SS'
         const filterDateString = `${ymd} ${hms}`;
 

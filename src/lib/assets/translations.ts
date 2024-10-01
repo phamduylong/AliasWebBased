b0be52445041caa0982930b07adb6e82a543bcac
longph <longphamduy2002@gmail.com>
2024-10-02 00:02:26 +0300

    translation resources file

---
+++ src/lib/assets/translations.ts
@@ -0,0 +1,35 @@
+export default {
+	en: {
+		'index_page.create_game': 'Create a new game',
+		'index_page.enter_game_code': 'Enter game code',
+		'index_page.external_url_not_allowed': 'External URLs are not allowed.',
+		'index_page.game_code_empty': "Game code is empty. Can't join.",
+		'index_page.join_game': 'Join game',
+		'nav.github': 'GitHub',
+		'nav.language': 'Language',
+		'nav.mode': 'Mode',
+		'nav.theme': 'Theme'
+	},
+	fi: {
+		'index_page.create_game': 'Luo uusi peli',
+		'index_page.enter_game_code': 'Syötä pelikoodi',
+		'index_page.external_url_not_allowed': 'Ulkoiset URL-osoitteet eivät ole sallittuja.',
+		'index_page.game_code_empty': 'Pelikoodi on tyhjä. Ei voi liittyä.',
+		'index_page.join_game': 'Liity peliin',
+		'nav.github': 'GitHub',
+		'nav.language': 'Kieli',
+		'nav.mode': 'Tila',
+		'nav.theme': 'Teema'
+	},
+	sv: {
+		'index_page.create_game': 'Skapa ett nytt spel',
+		'index_page.enter_game_code': 'Ange spelkod',
+		'index_page.external_url_not_allowed': 'Externa URL:er är inte tillåtna.',
+		'index_page.game_code_empty': 'Spelkoden är tom. Kan inte gå med.',
+		'index_page.join_game': 'Gå med i spelet',
+		'nav.github': 'GitHub',
+		'nav.language': 'Språk',
+		'nav.mode': 'Läge',
+		'nav.theme': 'Tema'
+	}
+};
